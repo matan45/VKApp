27db1591da4ec89135e3c9c50a3cc1861df33dd0
matan migdal <matan33214@gmail.com>
2024-10-07 18:03:34 +0300

    add content browser

--- VFEngine/editor/run/Main.cpp
+++ VFEngine/editor/run/Main.cpp
@@ -2,6 +2,7 @@
 #include "../handlers/EditorHandler.hpp"
 
 int main(int argc, char* argv[]) {
+
 	//need to create luncher in javaFX and send path to the project
 	//need to read the project config and set the level and content path
 	handlers::EditorHandler editorHandler;
--- VFEngine/editor/windows/ContentBrowser.hpp
+++ VFEngine/editor/windows/ContentBrowser.hpp
@@ -2,14 +2,42 @@
 #include "imguiHandler/ImguiWindow.hpp"
 #include "imgui.h"
 
+
+#include <string>
+#include <filesystem>
+namespace fs = std::filesystem;
+
 namespace windows {
+
+	struct Asset {
+		std::wstring name;
+		std::wstring path;
+		enum class AssetType { Texture, Model, Shader, Other } type;
+	};
+
+
 	class ContentBrowser : public interface::imguiHandler::ImguiWindow
 	{
+	private:
+		std::vector<Asset> assets;
+		fs::path currentPath = "c:\\matan";
 	public:
-		explicit ContentBrowser() = default;
+		explicit ContentBrowser() {
+			navigateTo(currentPath);
+		};
 		~ContentBrowser() override = default;
 
 		void draw() override;
+
+	private:
+		void navigateTo(const fs::path& path) {
+			if (fs::exists(path) && fs::is_directory(path)) {
+				currentPath = path;
+				loadDirectory(currentPath);
+			}
+		}
+
+		void loadDirectory(const fs::path& path);
 	};
 }
 
